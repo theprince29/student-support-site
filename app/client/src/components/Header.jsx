f8a97d2ffd5af6a217b784df6dc128e14f434a80
ankur0904 <ankursingh91002@gmail.com>
2023-08-03 22:06:28 +0530

    makes the hero section button working

--- app/client/src/components/Header.jsx
+++ app/client/src/components/Header.jsx
@@ -10,25 +10,24 @@ function Header() {
             class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
           >
             <svg class="bi me-2" width="40" height="32">
-              {/* <use xlinkHref="#bootstrap"></use> */}
             </svg>
             <span class="fs-4">CSE-AI</span>
           </a>
 
           <ul class="nav nav-pills">
             <li class="nav-item">
-              <a href="#" class="nav-link active" aria-current="page">
+              <a href="/xyz" class="nav-link active" aria-current="page">
                 Home
               </a>
             </li>
             <li class="nav-item">
-              <a href="#" class="nav-link">
-                Features
+              <a href="/xyz" class="nav-link">
+                Github
               </a>
             </li>
             <li class="nav-item">
-              <a href="#" class="nav-link">
-                Pricing
+              <a href="/xyz" class="nav-link">
+                About Us
               </a>
             </li>
           </ul>
